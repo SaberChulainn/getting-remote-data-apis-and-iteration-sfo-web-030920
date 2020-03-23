95026b975811a255c348dbd84f38570a945ac6e8
Ty Bandachit <tbandachit@gmail.com>
2020-03-23 20:11:26 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,14 +2,26 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# puts "Enter character name"
+# character = gets.chomp!
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  # binding.pry
+  # puts "."
+  films = []
+   name = response_hash['results'].select {|character| character["name"].downcase == character_name}
+   name[0]['films'].each do |film|
+    parse = RestClient.get(film)
+    films << JSON.parse(parse)
+  end
+  return films
+  
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
+  # collect those film API urls, make a web request to each UppRL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
@@ -19,6 +31,8 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
+  films.each {|film| puts film["title"]}
+
   # some iteration magic and puts out the movies in a nice list
 end
 
@@ -26,7 +40,6 @@ def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
-
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
